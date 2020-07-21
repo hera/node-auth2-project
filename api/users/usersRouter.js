c96c945e43fa796dfdd4a91869047c68ed0a018b
hera <a.heraimenka@gmail.com>
2020-07-21 08:45:32 -0700

    Get all users

--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const userDb = require("./usersModel");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(501).send("Not implemented");
+    userDb.getAll()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "Server error. Could not get all users.",
+                description: error
+            });
+        });
 });
 
 module.exports = router;
